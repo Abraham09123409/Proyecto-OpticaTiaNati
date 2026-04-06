69d70e7a38354d22da71b782b400645296281e02
Denof <102300721+Abraham09123409@users.noreply.github.com>
2026-04-06 00:15:55 -0400

    listo espero

--- Optica/Optica/settings.py
+++ Optica/Optica/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://abraham091234.pythonanywhere.com"
+]
