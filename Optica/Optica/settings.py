ab9bf719dc94b27b23a481d50435b787c3f3650d
Denof <102300721+Abraham09123409@users.noreply.github.com>
2026-04-05 23:18:23 -0400

    ultimos cambios

--- Optica/Optica/settings.py
+++ Optica/Optica/settings.py
@@ -25,9 +25,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-f-las+$b-8ebl-@a0%0^ri@r7i@$yt6c%!9n@-o=^#4eh((wy!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['abraham091234.pythonanywhere.com']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "app/static",
